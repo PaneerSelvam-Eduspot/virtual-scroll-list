65618bcbc7ef49b0372bcf0e15dd1839528f5098
PaneerSelvam-Eduspot <paneerselvamv4@gmail.com>
2025-08-29 16:06:08 +0530

    Updated project files

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './styles/App.css';
-import UserList from './components/UserList/UserList';
-function App() {
-    return (_jsx("div", { children: _jsx(UserList, {}) }));
-}
-export default App;
--- src/components/VirtualizedList/Row.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import './Row.css'; // Import CSS for Row
-const Row = React.memo(({ height, children }) => (_jsx("div", { className: "row", style: { height }, children: children })));
-export default Row;
